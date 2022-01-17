ff0f1630e2341995177356a1698c17122b449dab
soumadittya <soumadittya@gmail.com>
2022-01-17 09:16:48 +0530

    Commit 4

--- 5_slices_1.go
+++ 5_slices_1.go
@@ -1,2 +1,20 @@
-// declaring and initializing slices
+// Unlike arrays, the length of a slice can grow and shrink as you see fit.
+
+package main
+
+import ("fmt")
+
+func main(){
+	// declare a slice
+	// this is a slice with 0 length and 0 capacity
+	slice_1 := []int{}
+	fmt.Println("slice_1 : ", slice_1)
+
+	// initialize a slice
+
+	slice_2 := []int{1, 2, 3, 4}
+	fmt.Println("slice_2 : ", slice_2)
+
+	
+}
 
---
+++ 5_slices_2.go
@@ -0,0 +1,21 @@
+// different functions in slices
+
+// len() function - returns the length 
+// of the slice (the number of elements in the slice)
+// cap() function - returns the capacity of the slice 
+// (the number of elements the slice can grow or shrink to)
+
+package main()
+
+import ("fmt")
+
+func main(){
+	slice_1 := []string{"John", "Jack", "Emily", "Kate"}
+
+	// cap() function
+	fmt.Println("Capacity of slice_1 : ", slice_1)
+
+	// len() function
+	fmt.Println
+}
+
